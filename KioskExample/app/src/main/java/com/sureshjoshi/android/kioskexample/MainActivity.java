d08161b05686ae52b97cdbacc55f8783b1a348df
Nagamanoj Vankadhara <vnmanoj@moveinsync.com>
2017-06-30 18:58:54 +0530

    .

--- KioskExample/app/src/main/java/com/sureshjoshi/android/kioskexample/MainActivity.java
+++ KioskExample/app/src/main/java/com/sureshjoshi/android/kioskexample/MainActivity.java
@@ -1,11 +1,15 @@
 package com.sureshjoshi.android.kioskexample;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.app.admin.DevicePolicyManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.pm.PackageInstaller;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -16,6 +20,10 @@ import android.widget.Toast;
 import com.sureshjoshi.android.kioskexample.utils.AppVersion;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,6 +33,8 @@ import timber.log.Timber;
 public class MainActivity extends Activity {
 
     private static final File DOWNLOAD_DIRECTORY = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    public static final String ACTION_INSTALL_COMPLETE = "com.sureshjoshi.android.kioskexample.INSTALL_COMPLETE";
+    public static final String ACTION_EXIT_KIOSK_MODE = "com.sureshjoshi.android.kioskexample.EXIT_KIOSK_MODE";
 
     @Bind(R.id.button_toggle_kiosk)
     Button mButton;
@@ -59,10 +69,20 @@ public class MainActivity extends Activity {
             int result = appVersion.compareTo(new AppVersion(AppVersion.getApplicationVersion(this)));
             if (result >= 1) {
                 Timber.d("Application %s is older than %s", applicationVersion, fileVersion);
-                final Intent installIntent = new Intent(Intent.ACTION_VIEW);
-                installIntent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
-                installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(installIntent);
+                try {
+                    FileInputStream fis = new FileInputStream(file);
+                    installPackage(getApplicationContext(), fis, "pathik.stw.installer");
+                } catch (IOException e) {
+                    Timber.d("Application installation failed. File problem : %s", e.getMessage());
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    Timber.d("Application installation failed. Other problem : %s", e.getMessage());
+                    e.printStackTrace();
+                }
+//                final Intent installIntent = new Intent(Intent.ACTION_VIEW);
+//                installIntent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+//                installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(installIntent);
                 break;
             } else if (result == 0) {
                 Timber.d("Application %s is same as %s", applicationVersion, fileVersion);
@@ -70,6 +90,44 @@ public class MainActivity extends Activity {
                 Timber.d("Application %s is newer than %s", applicationVersion, fileVersion);
             }
         }
+    }
+
+    public static boolean installPackage(Context context, InputStream in, String packageName)
+            throws IOException {
+        Timber.d("Application %s installation started", packageName);
+        PackageInstaller packageInstaller = context.getPackageManager().getPackageInstaller();
+        PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(
+                PackageInstaller.SessionParams.MODE_FULL_INSTALL);
+        params.setAppPackageName(packageName);
+        // set params
+        int sessionId = packageInstaller.createSession(params);
+        PackageInstaller.Session session = packageInstaller.openSession(sessionId);
+        OutputStream out = session.openWrite("COSU", 0, -1);
+        byte[] buffer = new byte[65536];
+        int c;
+        while ((c = in.read(buffer)) != -1) {
+            out.write(buffer, 0, c);
+        }
+        session.fsync(out);
+        in.close();
+        out.close();
+        Timber.d("Application %s installation completed", packageName);
+        session.commit(createIntentSender(context, sessionId));
+        return true;
+    }
+
+    private static IntentSender createIntentSender(Context context, int sessionId) {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context,
+                sessionId,
+                new Intent(ACTION_INSTALL_COMPLETE),
+                0);
+        return pendingIntent.getIntentSender();
+    }
+
+    @OnClick(R.id.button_remove_admin)
+    public void clearDeviceAdminPermission() {
+        mDpm.clearDeviceOwnerApp("com.sureshjoshi.android.kioskexample");
     }
 
 
@@ -99,8 +157,12 @@ public class MainActivity extends Activity {
         }
 
         mDecorView = getWindow().getDecorView();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_INSTALL_COMPLETE);
+        intentFilter.addAction(ACTION_EXIT_KIOSK_MODE);
+        registerReceiver(mIntentReceiver, intentFilter);
 
-        mWebView.loadUrl("http://www.vicarasolutions.com/");
+        mWebView.loadUrl("http://www.moveinsync.com/");
     }
 
     @Override
@@ -142,4 +204,35 @@ public class MainActivity extends Activity {
             // TODO: Log and handle appropriately
         }
     }
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (ACTION_INSTALL_COMPLETE.equals(action)) {
+                int result = intent.getIntExtra(PackageInstaller.EXTRA_STATUS,
+                        PackageInstaller.STATUS_FAILURE);
+                String packageName = intent.getStringExtra(PackageInstaller.EXTRA_PACKAGE_NAME);
+                Timber.d("PackageInstallerCallback: result= %s, packageName = %s", result, packageName);
+                switch (result) {
+                    case PackageInstaller.STATUS_PENDING_USER_ACTION: {
+                        // this should not happen in M, but will happen in L and L-MR1
+                        startActivity((Intent) intent.getParcelableExtra(Intent.EXTRA_INTENT));
+                    }
+                    break;
+                    case PackageInstaller.STATUS_SUCCESS: {
+                        Timber.d("Package %s installation complete", packageName);
+                    }
+                    break;
+                    default: {
+                        Timber.e("Install failed.");
+                        return;
+                    }
+                }
+            } else if (ACTION_EXIT_KIOSK_MODE.equals(action)) {
+                enableKioskMode(false);
+            }
+        }
+    };
 }
